2bb3e6095b99b23937fac20b33bc1240de03c12e
Sandra Moya <sanmilmo@gmail.com>
2020-07-18 01:45:54 -0400

    set my state

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,22 +1,38 @@
-import React from 'react'
-import Header from "./Header"
-import axios from "axios"
-
-
-const API_KEY = "017359035b9fc9c55068649bcf9a5ff5"
-
+import React, { useState } from "react";
+import Header from "./Header";
+import axios from "axios";
+import Content from "./Content";
+import WeatherSearch from "./WeatherSearch";
+import WeatherData from "./WeatherData";
+import Context from "../Context";
 
 const Main = () => {
+  const [weather, setWeather] = useState();
+  const api_call = async (e) => {
+    //prevent page to refrest when form is submitted
+    e.preventDefault();
+    const API_KEY = "017359035b9fc9c55068649bcf9a5ff5";
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=Montreal&appid=${API_KEY}&units=metric`;
+    //template literals
+    const request = axios.get(url);
+    const response = await request;
+    setWeather(response.data.main);
+  };
 
-    const api_call = () => {
-        const url = `api.openweathermap.org/data/2.5/weather?q=Liverpool&appid=${API_KEY}`
-        //template literals 
-    }
-    return (
-        <div className="main">
-            <Header />
-        </div>
-    )
-}
+  weather && console.log(weather);
 
-export default Main 
+  return (
+    <div className='main'>
+      <Header />
+      <Content>
+        <Context.Provider value={{api_call, weather}}>
+            {/* if names of key and value are the same, "api_call: api_call"I can remove it   */}
+          <WeatherSearch />
+          {weather && <WeatherData />}
+        </Context.Provider>
+      </Content>
+    </div>
+  ); 
+};
+
+export default Main;
